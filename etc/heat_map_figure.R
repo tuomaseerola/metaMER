04b8eabc331e003b0b7210f50232407947161c91
Cameron Anderson <andersoc@mcmaster.ca>
2025-03-07 16:18:51 -0500

    manuscript: add heatmap figure

--- etc/heat_map_figure.R
+++ etc/heat_map_figure.R
@@ -1,20 +1,16 @@
 
-# Load libraries --------------------------------------------------
-
-library(ggplot2)
-
 
 # Read in best models ---------------------------------------------
 
 
 
-R_summary <- read.csv("analysis/R_summary.csv")
-C_summary <- read.csv("analysis/C_summary.csv")
+R_heatmap <- read.csv("analysis/R_summary.csv")
+C_heatmap <- read.csv("analysis/C_summary.csv")
 
 
 # Select relevant columns for plotting ----------------------------
 
-Rtmp <- R_summary |> dplyr::select(
+Rtmp <- R_heatmap |> dplyr::select(
   model_class_id,
   feature_n_complexity,
   valuesMax,
@@ -23,7 +19,7 @@ Rtmp <- R_summary |> dplyr::select(
   citekey
 )
 
-Ctmp <- C_summary |> dplyr::select(
+Ctmp <- C_heatmap |> dplyr::select(
   model_class_id,
   feature_n_complexity,
   valuesMax,
@@ -34,6 +30,12 @@ Ctmp <- C_summary |> dplyr::select(
 
 
 tmp <- rbind(Rtmp, Ctmp)
+
+tmp$model_class_id
+
+tmp[tmp$model_class_id %in% 
+      c("Support Vector Machines",
+        "Tree-based Methods" ),]$model_class_id <-"SVM,TM"
 
 tmp <- tmp |> dplyr::group_by(
   dimension,
@@ -54,7 +56,7 @@ tmp$dimension <- factor(tmp$dimension,
                                    "arousal", 
                                    "classification"))
 
-tmp |>
+heatmap_facets <- tmp |>
   ggplot(aes(x = model_class_id, 
              y = feature_n_complexity,
              fill = valuesMax
@@ -63,11 +65,47 @@ tmp |>
   facet_wrap(facets = vars(dimension))+
   scale_fill_distiller(name = "Accuracy", palette = "Spectral")+
 #  scale_fill_gradient2(name = "Accuracy")+
-  scale_x_discrete(labels = c("LM", "NN", "SVM", "TM"))+
+  scale_x_discrete(labels = c("LM", "NN", "Other"))+
   scale_y_discrete(labels = c("<30", "30-300", ">300"))+
   geom_text(aes(label = label))+
-  labs(x = "Model Class Type", y = "Feature N Category")+
+  labs(x = "Model class type", y = "Feature N category")+
   theme_classic()
 
 
+heatmap_facets
+
 ggsave("heat_map_figure.png", width = 14, height = 4, units = "in")
+
+
+
+# Now separately plot concepts ----------------------------------
+
+heatmap_fig <- function(df, dim) {
+  df <- subset(df, dimension == dim)
+    df |> 
+      ggplot(aes(x = model_class_id, 
+               y = feature_n_complexity,
+               fill = valuesMax
+    )) +
+    geom_raster()+
+    scale_fill_distiller(name = "Accuracy", palette = "Spectral")+
+    scale_x_discrete(labels = c("LM", "NN", "SVM", "TM"))+
+    scale_y_discrete(labels = c("<30", "30-300", ">300"))+
+    geom_text(aes(label = label))+
+    labs(x = "Model class type", y = "Feature N category")+
+    theme_classic()
+
+}
+
+
+heatmap_valence <- heatmap_fig(tmp, "valence")
+heatmap_arousal <- heatmap_fig(tmp, "arousal")
+heatmap_classification <- heatmap_fig(tmp, "classification")
+
+save(heatmap_valence,
+     heatmap_arousal,
+     heatmap_classification,
+     heatmap_facets,
+     file = "heatmap_figs.RData")
+
+
